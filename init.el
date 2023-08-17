e8397f447f4027242ee111de31f5bbbe232d982a
KueiYi Lee <gotomon365@gmail.com>
2023-08-17 10:11:56 +0800

    add emacs org mode

--- init.el
+++ init.el
@@ -31,15 +31,15 @@
 (require 'init-custom)
 (require 'init-tab)
 (require 'init-python)
-
+(require 'init-org)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-    '(package-selected-packages
-         '(embark marginalia orderless vertico zenburn-theme use-package evil-visual-mark-mode evil-multiedit)))
+ '(package-selected-packages
+      '(embark marginalia orderless vertico zenburn-theme use-package evil-visual-mark-mode evil-multiedit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
