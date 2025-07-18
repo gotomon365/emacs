42746c86dcc0efe1197d77574e6bf168f35faceb
kuei <kuei@debian>
2025-07-18 23:27:37 +0800

    add theme setting

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,4 +1,10 @@
 (load-theme 'wombat t)
 (set-face-attribute 'default nil :height 100)
+(custom-theme-set-faces
+    'wombat
+    '(cursor ((t (:background "#abb2bf")))))
+
+(enable-theme 'wombat)
+
 
 (provide 'init-theme)
