1cdc4c6b9b467d019677dbca255876255981036a
KueiYi <gotomon365@gmail.com>
2026-04-14 01:25:26 +0800

    d duplicate files

--- lisp/lisp/init-org.el
+++
@@ -1,9 +0,0 @@
-(add-hook 'org-mode-hook 'org-indent-mode)
-(with-eval-after-load 'evil-maps (define-key evil-motion-state-map (kbd "TAB") nil))
-
-
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-
-(provide 'init-org)
